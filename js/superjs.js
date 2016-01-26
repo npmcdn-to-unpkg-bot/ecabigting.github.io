384b12efa1bcabe8ecfce3a93badf6035d57e43c
Eric Thomas Cabigting <ericcabigting@outlook.com>
2016-01-26 15:07:12 +0400

    added animation
    - added animate.min.css
    - started coding for skills section
    - added changes to superjs.js

--- js/superjs.js
+++ js/superjs.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
-  $('#fullpage').fullpage({
-    anchors: ['home', 'projects', 'skills','qualifications'],
-    menu: '#myMenu',
-    verticalCentered: true,
-    resize: true
-  });
-    $(".navbar-nav li a").click(function(event) {
-      $(".navbar-collapse").collapse('hide');
-    });
+      $('#fullpage').fullpage({
+        anchors: ['home', 'projects', 'skills','qualifications'],
+        menu: '#myMenu',
+        verticalCentered: true,
+        resize: true
+      });
+      $(".navbar-nav li a").click(function(event) {
+        $(".navbar-collapse").collapse('hide');
+      });
+
+      setTimeout(function() {
+        $('#bossContacts').addClass('animated fadeIn');
+        $('#bossPic').addClass('animated fadeInDown');
+        $('#bossName').addClass('animated fadeInRight');
+        $('#bossTitle').addClass('animated fadeInLeft');
+        $('#bossEmail').addClass('animated slideInUp');
+        $('#bossPhone').addClass('animated slideInUp');
+        $('#bossAddress').addClass('animated slideInUp');
+      },800);
 });
